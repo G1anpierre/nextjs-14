01b207cfddb2c7b861b60176be7f94949af1963f
G1anpierre <fgianpierre@gmail.com>
2023-11-07 16:30:37 +0100

    complete nextjs 14 course

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,12 @@ import {InvoicesTableSkeleton} from '@/app/ui/skeletons'
 import {fetchInvoicesPages} from '@/app/lib/data'
 import Pagination from '@/app/ui/invoices/pagination'
 
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+}
+
 const InvoicesPage = async ({
   searchParams,
 }: {
@@ -18,7 +24,6 @@ const InvoicesPage = async ({
   const query = searchParams?.query || ''
   const currentPage = Number(searchParams?.page) || 1
   const totalPages = await fetchInvoicesPages(query)
-  console.log('totalPages :', totalPages)
 
   return (
     <div className="w-full">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,37 @@
 import '@/app/ui/global.css'
 import {inter} from '@/app/ui/fonts'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    url: 'https://next-learn-dashboard.vercel.sh',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  keywords: ['nextjs', 'dashboard', 'learn'],
+}
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
